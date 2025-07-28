1662793951fa319815767276b5faa12f434604f8
joaooliveira247 <joaooliveira247@gmail.com>
2025-07-28 20:51:02 -0300

    feat(config): add func LoadEnv to load DBURL 'const'

--- src/config/config.go
+++ src/config/config.go
@@ -1,5 +1,31 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 var (
 	DBURL = ""
 )
+
+func LoadEnv() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal("Error when load .env file")
+	}
+
+	DBURL = fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%s sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSL"),
+	)
+}
