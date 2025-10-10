8ade65b43e83c5b7d0fa5fba85fc38025f48134e
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-10 17:29:52 -0300

    feat(errors): add Error func implementations go default interface error

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -1,6 +1,12 @@
 package errors
 
+import "fmt"
+
 type BaseError struct {
 	Resource string
 	Msg      string
+}
+
+func (err *BaseError) Error() string {
+	return fmt.Sprintf("(%s): %s", err.Resource, err.Msg)
 }
