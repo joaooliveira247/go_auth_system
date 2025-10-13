a0f6de48caca4b721e9006565df1043280bb2573
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-13 11:15:40 -0300

    refactor(hash): change base error in GenHash

--- src/security/hash.go
+++ src/security/hash.go
@@ -1,12 +1,15 @@
 package security
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"github.com/joaooliveira247/go_auth_system/src/errors"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func GenHash(passwd string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
 
 	if err != nil {
-		return "", err
+		return "", errors.NewHashError(err)
 	}
 
 	return string(hash), nil
