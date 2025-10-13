ffaabb1324cc6fa138b564a000d65bef45edeb0b
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-13 10:16:09 -0300

    test(hash): add success case test GenHashSuccess

---
+++ tests/security/hash_test.go
@@ -0,0 +1,17 @@
+package security_test
+
+import (
+	"testing"
+
+	"github.com/joaooliveira247/go_auth_system/src/security"
+	"github.com/joaooliveira247/go_auth_system/tests/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func GenHashSuccess(t *testing.T) {
+	passwd := mocks.GenFakePassword()
+	hash, err := security.GenHash(passwd)
+
+	assert.NoError(t, err)
+	assert.Len(t, hash, 60)
+}
