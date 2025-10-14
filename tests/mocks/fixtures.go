32ea76c64daab459adf801da37d99550863bc9eb
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-14 18:43:11 -0300

    test(fixture): add SetupMockDB() func

--- tests/mocks/fixtures.go
+++ tests/mocks/fixtures.go
@@ -1,8 +1,32 @@
 package mocks
 
 import (
+	"log"
+
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-faker/faker/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func SetupMockDB() (*gorm.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gormDB, err := gorm.Open(
+		postgres.New(postgres.Config{Conn: db}),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return gormDB, mock
+}
 
 func GenFakePassword() string {
 	return faker.Password()
