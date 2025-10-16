44416cdeb65dad4e416fa3e0fb3b7862ec213d08
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-16 18:17:40 -0300

    feat(repositories): implement GetUserByEmail Method in userRepository struct

--- src/repositories/user.go
+++ src/repositories/user.go
@@ -30,3 +30,15 @@ func (repository *userRepository) Create(
 
 	return user.ID, nil
 }
+
+func (repository *userRepository) GetUserByEmail(
+	email string,
+) (models.UserModel, error) {
+	var user models.UserModel
+
+	if result := repository.db.First(&user, "email = ?", email); result.Error != nil {
+		return models.UserModel{}, errors.NewDatabaseError(result.Error)
+	}
+
+	return user, nil
+}
