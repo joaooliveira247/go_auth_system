a9c190bae3f130fe15d6c00a7dbcee43eb0226e5
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-24 18:34:00 -0300

    feat(schemas): add ToCacheJson method in UserSchemaIn struct

--- src/schemas/user.go
+++ src/schemas/user.go
@@ -1,7 +1,32 @@
 package schemas
 
+import (
+	"encoding/json"
+
+	"github.com/joaooliveira247/go_auth_system/src/errors"
+	"github.com/joaooliveira247/go_auth_system/src/security"
+)
+
 type UserSchemaIn struct {
 	Email           string `json:"email"`
 	Password        string `json:"password"`
 	ConfirmPassword string `json:"-"`
+}
+
+func (schema *UserSchemaIn) ToCacheJson() (string, error) {
+	hashedPassword, err := security.GenHash(schema.Password)
+
+	if err != nil {
+		return "", errors.NewCacheError(err)
+	}
+
+	schema.Password = hashedPassword
+
+	schemaJson, err := json.Marshal(schema)
+
+	if err != nil {
+		return "", errors.NewCacheError(err)
+	}
+
+	return string(schemaJson), nil
 }
