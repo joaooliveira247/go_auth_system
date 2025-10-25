10205ea6b7b82959e142c422765c617e6ec2acc9
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-25 19:07:58 -0300

    test(schemas): change GenFakeUserSchemaIn() call in TestToCacheJsonSuccess

--- tests/schemas/user_test.go
+++ tests/schemas/user_test.go
@@ -4,23 +4,16 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/joaooliveira247/go_auth_system/src/schemas"
 	"github.com/joaooliveira247/go_auth_system/tests/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestToCacheJsonSuccess(t *testing.T) {
 
-	user := mocks.GenFakeUser()
-
-	schema := schemas.UserSchemaIn{
-		Email:           user.Email,
-		Password:        user.Password,
-		ConfirmPassword: user.Password,
-	}
+	schema := mocks.GenFakeUserSchemaIn()
 
 	schemaJson, err := schema.ToCacheJson()
 
 	assert.NoError(t, err)
-	assert.Contains(t, schemaJson, fmt.Sprintf(`"email":"%s"`, user.Email))
+	assert.Contains(t, schemaJson, fmt.Sprintf(`"email":"%s"`, schema.Email))
 }
