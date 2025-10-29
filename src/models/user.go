569f1bb7276adc87638723fa987dfdc7944d63a3
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-29 17:17:38 -0300

    feat(models): add json tags in UserModel struct

--- src/models/user.go
+++ src/models/user.go
@@ -6,8 +6,8 @@ import (
 
 type UserModel struct {
 	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid();column:id"`
-	Email     string    `gorm:"type:varchar(255);not null;unique;column:email"`
-	Password  string    `gorm:"type:varchar(60); not null;column:password"`
+	Email     string    `gorm:"type:varchar(255);not null;unique;column:email"           json:"email"`
+	Password  string    `gorm:"type:varchar(60); not null;column:password"               json:"password"`
 	Role      string    `gorm:"type:varchar(30);default:user;column:role"`
 	CreatedAt int64     `gorm:"autoCreateTime;type:timestamp;column:created_at"`
 	UpdatedAt int64     `gorm:"autoUpdateTime;type:timestamp;column:updated_at"`
