d3b823866695f14729caafe31b07fd4be44f7b59
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-31 18:06:53 -0300

    feat(cache): implement Set method for redisCache struct

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -1,6 +1,13 @@
 package cache
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"time"
+
+	"github.com/joaooliveira247/go_auth_system/src/config"
+	"github.com/joaooliveira247/go_auth_system/src/errors"
+	"github.com/redis/go-redis/v9"
+)
 
 type Cache interface {
 	Set(key string, value interface{}) error
@@ -12,4 +19,16 @@ type redisCache struct {
 
 func NewCache(client *redis.Client) Cache {
 	return &redisCache{client}
+}
+
+func (redisCache *redisCache) Set(key string, value interface{}) error {
+	ctx := context.Background()
+	if err := redisCache.client.Set(
+		ctx,
+		key,
+		value,
+		time.Duration(config.CacheDuration)*time.Second).Err(); err != nil {
+		return errors.NewCacheError(err)
+	}
+	return nil
 }
