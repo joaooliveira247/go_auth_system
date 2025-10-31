df993f1ab427d8a2dbe495062c07500b171f1cee
joaooliveira247 <joaooliveira247@gmail.com>
2025-10-31 17:53:41 -0300

    test(cache): add test cases fail TestCacheSetFail

--- tests/cache/cache_test.go
+++ tests/cache/cache_test.go
@@ -1,11 +1,13 @@
 package cache
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
 
 	"github.com/joaooliveira247/go_auth_system/src/cache"
+	"github.com/joaooliveira247/go_auth_system/src/errors"
 	"github.com/joaooliveira247/go_auth_system/src/security"
 	"github.com/joaooliveira247/go_auth_system/tests/mocks"
 	"github.com/stretchr/testify/assert"
@@ -28,4 +30,44 @@ func TestCacheSetSuccess(t *testing.T) {
 
 	assert.NoError(t, err)
 
+}
+
+func TestCacheSetFail(t *testing.T) {
+	cacheDB, mock := mocks.SetupMockCache()
+
+	testCases := []struct {
+		testName          string
+		err               error
+		errStringExpected string
+	}{
+		{
+			testName:          "Context Fail",
+			err:               context.Canceled,
+			errStringExpected: "(Cache): context canceled",
+		},
+		{
+			testName:          "UnmappedError",
+			err:               errors.ErrNotExpected,
+			errStringExpected: "(Cache): NotExpectedTestError",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			user := mocks.GenFakeUserSchemaIn()
+
+			userJson, _ := user.ToCacheJson()
+
+			userSignUpToken, _ := security.GenerateSignUpToken(16)
+
+			mock.ExpectSet(fmt.Sprintf("signup:%s", userSignUpToken), userJson, time.Second*7200).SetErr(testCase.err)
+
+			cache := cache.NewCache(cacheDB)
+
+			err := cache.Set(fmt.Sprintf("signup:%s", userSignUpToken), userJson)
+
+			assert.Error(t, err)
+			assert.Equal(t, testCase.errStringExpected, err.Error())
+		})
+	}
 }
